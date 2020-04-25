d4e38690b8536c7bb733b0758e2cbb878c339ac9
Vitalii <50666393+vitastolo@users.noreply.github.com>
2020-04-25 20:26:09 +0700

    add some Vuejs code

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 url = "https://arcane-depths-61381.herokuapp.com/"
+//
+// let title;
+// let contacts;
 
-let title;
-let contacts;
-
-const company = {
-  title: 'Какая-то',
-  contacts: 'Какие-то',
-  infoCompany: (company) => {
-    title = `Название компании: ${company.title}`;
-    contacts = `Сайт компании: ${company.contacts}`;
-
-    let divTitle = document.createElement('div');
-    let divContacts = document.createElement('div');
-
-    divTitle.className = "listOfCompany";
-    divContacts.className = "listOfCompany";
-
-    divTitle.innerHTML = `${title}`;
-    divContacts.innerHTML =`${contacts}`;
-
-    document.body.append(divTitle);
-    document.body.append(divContacts);
-
+// const company = {
+//   title: 'Какая-то',
+//   contacts: 'Какие-то',
+//   infoCompany: (company) => {
+//     title = `Название компании: ${company.title}`;
+//     contacts = `Сайт компании: ${company.contacts}`;
+//
+//     let divTitle = document.createElement('div');
+//     let divContacts = document.createElement('div');
+//
+//     divTitle.className = "listOfCompany";
+//     divContacts.className = "listOfCompany";
+//
+//     divTitle.innerHTML = `${title}`;
+//     divContacts.innerHTML =`${contacts}`;
+//
+//     document.body.append(divTitle);
+//     document.body.append(divContacts);
+//
+//   }
+// }
+let infoCompany = new Vue({
+  el: '#infoCompany',
+  data: {
+    items: [
+    ]
   }
-}
+})
 
 async function loadData(url = '') {
   const response = await fetch(url);
   const data = await response.json();
-  return company.infoCompany(data);
+  infoCompany.items.push({ message: data.title });
+  infoCompany.items.push({ message: data.contacts });
+
+  return data;
 }
 
 function init() {
